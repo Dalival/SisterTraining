d261c0989636256ebd0035f1df1f988410fbeee1
Dalival <egorfedorenko.w@gmail.com>
2022-02-23 13:15:46 +0300

    Delete non-task projects

--- School/SchoolProject/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using SchoolProject;
-
-var sonya = new Student("Sofia", "Fedorenko", "23.09.2006", 9, new List<int> {9, 9, 8, 7});
-Console.WriteLine("Second name: " + sonya.SecondName);
-sonya.SecondName = "Spirina";
